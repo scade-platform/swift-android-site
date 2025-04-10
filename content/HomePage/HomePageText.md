ec1d1997507abadcae5994aec739dbbe318cc999
Grigory Markin <gr.markin@gmail.com>
2025-04-10 20:55:08 +0200

    fix static export for images

--- content/HomePage/HomePageText.md
+++ content/HomePage/HomePageText.md
@@ -4,6 +4,6 @@ link: "#2"
 date: 06 August 2020
 ---
 
-ll Swift 5.9 support for
+Full Swift 6 support for
 
-arm64 / x86_64 / arm / x86Fu
+arm64 / x86_64 / arm / x86
--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,3 @@
-/*
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/
-});
-
-module.exports = withMDX({
-  // Existing Next.js configuration
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  // output: 'export'
-});
-*/
-
 import { createMDX } from 'fumadocs-mdx/next';
 
 const withMDX = createMDX();
@@ -17,6 +5,10 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true, // Disable next/image optimization for static exports
+  },
 };
 
 export default withMDX(config);
