f1b97cff3d9ed59521a831259c235a1b4d9bb1fb
Grigory Markin <gr.markin@gmail.com>
2025-04-10 19:42:08 +0200

    fix previous commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,14 @@
+/*
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/
+});
+
+module.exports = withMDX({
+  // Existing Next.js configuration
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  // output: 'export'
+});
+*/
 
 import { createMDX } from 'fumadocs-mdx/next';
 
