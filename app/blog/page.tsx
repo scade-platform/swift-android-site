b661e8cde96f3107bd62816f895a2319de5cd952
Oleksandr Dmukhovskyi <olexandrbig@gmail.com>
2025-07-18 11:29:15 +0300

    Footer in blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,24 +1,28 @@
 import DynamicTextField from '@/components/DynamicTextField';
 import getDynamicTextFieldMetadata from '@/utils/getDynamicTextField';
+import Footer from "@/components/Footer";
 
 
 export default function SwiftForAndroid() {
   const postMatadata = getDynamicTextFieldMetadata("content/Blog")
 
   return (
-    <div
-      className="pt-16 grid grid-rows-[1fr] items-center justify-items-center p-8 font-[family-name:var(--font-geist-sans)]">
+    <div>
+      <div
+        className="pt-16 grid grid-rows-[1fr] items-center justify-items-center p-8 font-[family-name:var(--font-geist-sans)]">
 
-      <main className="flex flex-col gap-8 row-start-1 items-center sm:items-start">
-        {<div className="container prose">
-          {postMatadata.map((post, postIndex) => {
-            return (
-              <DynamicTextField key={postIndex} post={post}/>
-            )
-          })}
-        </div>}
-      </main>
+        <main className="flex flex-col gap-8 row-start-1 items-center sm:items-start">
+          {<div className="container prose">
+            {postMatadata.map((post, postIndex) => {
+              return (
+                <DynamicTextField key={postIndex} post={post}/>
+              )
+            })}
+          </div>}
+        </main>
 
+      </div>
+      <div className="flex items-center m-[auto]"><Footer/></div>
     </div>
 
   );
