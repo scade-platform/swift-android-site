e9c27f89a5217e9100cadbdb6fb985604dfeb79e
olexandrbig <olexandrbig@users.noreply.github.com>
2025-07-18 08:30:21 +0000

    deploy: b661e8cde96f3107bd62816f895a2319de5cd952

---
+++ _next/static/chunks/app/blog/page-0ab97e1a8a3111c4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[831],{18902:()=>{},32308:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,18902,23)),Promise.resolve().then(n.t.bind(n,6874,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[559,874,441,684,358],()=>s(32308)),_N_E=e.O()}]);
